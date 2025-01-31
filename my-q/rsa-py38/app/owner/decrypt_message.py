ae116001335ae4fd8a9050767a4a97d320f610c7
Brandon R. India <brandonindia@microsoft.com>
2025-01-31 16:09:24 -0600

    fixed bug

--- my-q/rsa-py38/app/owner/decrypt_message.py
+++ my-q/rsa-py38/app/owner/decrypt_message.py
@@ -2,9 +2,32 @@ import shutil
 # Source path
 source = 'owner_public_key.pem'
 
+private_key_path = "./owner_private_key.pem"
+public_key_path = "./owner_public_key.pem"
 # Destination path
-destination = '../sender/owner_public_key.pem'
+destination = './owner_public_key.pem'
+def decrypt_message(private_key_path, ciphertext_file_path):
+  with open(private_key_path, "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,  # If the key is encrypted, provide the password here
+        backend=default_backend()
+    )
 
+    # Read the ciphertext from a file
+    with open(ciphertext_file_path, "rb") as f:
+        ciphertext = f.read()
+
+  plaintext = private_key.decrypt(
+      ciphertext,
+      padding.OAEP(
+          mgf=padding.MGF1(algorithm=hashes.SHA256()),
+          algorithm=hashes.SHA256(),
+          label=None
+      )
+  )
+
+  return plaintext
 # Copy the file
 shutil.copy(source, destination)
 
